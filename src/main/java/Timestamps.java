53711138eaa39ece9b4cc1d02a5840f2baf3fc97
Lauren De Los Santos <lauren.301371@nv.ccsd.net>
2020-09-04 17:00:22 +0000

    I completed Timestamps

--- src/main/java/Timestamps.java
+++ src/main/java/Timestamps.java
@@ -14,12 +14,23 @@ class Timestamps {
         int minutes2 = scanner.nextInt();
         int seconds2 = scanner.nextInt();
 
+        int hours3;
+        int minutes3;
+        int seconds3;
 
         /*
          *  your code goes here
          */
+          hours3 = hours2 - hours1;
+          minutes3 = minutes2 - minutes1;
+          seconds3 = seconds2 - seconds1;
+          
+          hours3 = hours3 * 3600;
+          minutes3 = minutes3 * 60;
 
+          seconds3 = hours3 + minutes3 + seconds3;
 
+          System.out.println(seconds3);
         // closing the scanner object
         scanner.close();
     }
